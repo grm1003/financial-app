7d197a8fa70f757d8a6814a4f3e27ac288fe95de
Gabriel Rezende Machado <47564540+grm1003@users.noreply.github.com>
2024-06-27 19:17:29 -0300

    Api Working, Merging with develop

--- src/main/java/com/financial/app/domain/resourses/CreateUser.java
+++
@@ -1,9 +0,0 @@
-package com.financial.app.domain.resourses;
-
-import com.financial.app.domain.entitys.UserDataModel;
-
-import java.util.Optional;
-
-public interface CreateUser {
-    public Optional<UserDataModel> createUser(UserDataModel user);
-}
